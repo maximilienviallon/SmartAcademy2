68cb6d6f1fb3ee148c2c263b9a38ba48986a4403
2nowfake <baba_220@hotmail.com>
2019-06-07 03:05:20 +0200

    Finished modification screens

--- src/controller/EducationModificationScreenController.java
+++ src/controller/EducationModificationScreenController.java
@@ -1,5 +1,12 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class
 EducationModificationScreenController {
+    public void EMBacToDetBut(ActionEvent actionEvent) {
+    }
+
+    public void EMRemBut(ActionEvent actionEvent) {
+    }
 }
--- src/controller/PermissionModificationScreenController.java
+++ src/controller/PermissionModificationScreenController.java
@@ -1,4 +1,11 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class PermissionModificationScreenController {
+    public void EMBacToDetBut(ActionEvent actionEvent) {
+    }
+
+    public void EMRemBut(ActionEvent actionEvent) {
+    }
 }
