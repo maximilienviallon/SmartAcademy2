375bdd1717a877c1523ab23871444b7673b34340
InLe0 <rockeater0@gmail.com>
2019-06-09 03:54:42 +0100

    contact overview working

--- src/Persistence/DBFacade.java
+++ src/Persistence/DBFacade.java
@@ -2,18 +2,18 @@ package Persistence;
 
 import Domain.Apprentices;
 import Domain.Companies;
+import Domain.Contacts;
 
-import java.sql.Date;
 import java.util.ArrayList;
 
 public class DBFacade {
 
-    public static boolean checkLogin (boolean doneLogin, String loginIDTextField, String logInPasswordID) {
+    public static boolean checkLogin(boolean doneLogin, String loginIDTextField, String logInPasswordID) {
 
         try {
 
             DB.selectSQL("select fldUsername from tblUsers");
-            do  {
+            do {
 
 
                 String username = DB.getData();
@@ -22,25 +22,24 @@ public class DBFacade {
                     System.out.println("Username has been found in DB: " + username);
 
                     do {
-                        DB.selectSQL("select fldPassword from tblUsers where fldUsername = '"+ loginIDTextField + "'");
+                        DB.selectSQL("select fldPassword from tblUsers where fldUsername = '" + loginIDTextField + "'");
                         String pass = DB.getData();
                         if (pass.equals(logInPasswordID)) {
                             System.out.println("Password is a match for given username...Login Done ");
                             return doneLogin == true;
                         } else System.out.println("Access Denied , wrong password");
-                    }while (true);
+                    } while (true);
                 }
-            }while(true);
+            } while (true);
 
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         System.out.println("Execute Done ");
         return false;
     }
 
-    public static String checkPermission(String userName){
+    public static String checkPermission(String userName) {
         DB.selectSQL("select fldPermission from tblPermissions where fldPermissionID in (select fldPermissionID from tblUserPermissionBridge where fldUsername = '" + userName + "')");
         String permissionTier = DB.getData();
         System.out.println(permissionTier);
@@ -48,27 +47,27 @@ public class DBFacade {
         return permissionTier;
     }
 
-    public static Integer[] checkYourPrivilege(String userName){
+    public static Integer[] checkYourPrivilege(String userName) {
         String query = ("select fldCompanyID from tblPermissions where fldPermissionID in (select fldPermissionID from tblUserPermissionBridge where fldUsername = '" + userName + "')");
         ArrayList<Object[]> privilegeQuery = DB.select(query);
         Integer[] companyPrivileges = new Integer[privilegeQuery.size()];
         int positionChecker = 0;
         for (Object[] objects : privilegeQuery) {
-            companyPrivileges[positionChecker]= (int) objects[0];
+            companyPrivileges[positionChecker] = (int) objects[0];
             positionChecker++;
         }
         return companyPrivileges;
     }
 
-    public static ArrayList<Companies> retrieveCompanies(Integer[] companyList){
+    public static ArrayList<Companies> retrieveCompanies(Integer[] companyList) {
         ArrayList<Companies> retrievedCompanies = new ArrayList<>();
 
         try {
             String query = ("select fldCompanyID, fldCompanyName, fldFieldOfExpertise, fldCompanyZipcode, fldCVRNum, fldPNum from tblCompanies where fldCompanyID in (");
-            for (int companyID:companyList) {
-                query+= (companyID + ",");
+            for (int companyID : companyList) {
+                query += (companyID + ",");
             }
-            query = query.substring(0,query.length()-1) + ")";
+            query = query.substring(0, query.length() - 1) + ")";
 
             ArrayList<Object[]> companyQuery = DB.select(query);
 
@@ -80,7 +79,7 @@ public class DBFacade {
                 String cvr = (String) objects[4];
                 String pNum = (String) objects[5];
 
-                Companies companies = new Companies(companyID,name,fieldOfExpertise,zipcode,cvr,pNum, null);
+                Companies companies = new Companies(companyID, name, fieldOfExpertise, zipcode, cvr, pNum, null);
 
                 retrievedCompanies.add(companies);
             }
@@ -93,11 +92,12 @@ public class DBFacade {
         return retrievedCompanies;
 
     }
-    public static ArrayList<Apprentices> retrieveApprentices(int selectedCompany){
+
+    public static ArrayList<Apprentices> retrieveApprentices(int selectedCompany) {
         ArrayList<Apprentices> retrievedApprentices = new ArrayList<>();
 
         try {
-            String query = ("select fldApprenticeCPR, fldApprenticeName, fldCompanyID, fldApprenticeEmail, fldApprenticePhone, fldApprenticeID from tblApprentices where fldCompanyID = "+ selectedCompany);
+            String query = ("select fldApprenticeCPR, fldApprenticeName, fldCompanyID, fldApprenticeEmail, fldApprenticePhone, fldApprenticeID from tblApprentices where fldCompanyID = " + selectedCompany);
             ArrayList<Object[]> apprenticeQuery = DB.select(query);
 
             for (Object[] objects : apprenticeQuery) {
@@ -119,6 +119,34 @@ public class DBFacade {
 
         }
         return retrievedApprentices;
+
+    }
+
+    public static ArrayList<Contacts> retrieveContacts(int selectedCompany) {
+        ArrayList<Contacts> retrievedContacts = new ArrayList<>();
+
+        try {
+            String query = ("select fldConID, fldCompanyID, fldConName, fldConEmail, fldConPhoneNo from tblContacts where fldCompanyID = " + selectedCompany);
+            ArrayList<Object[]> apprenticeQuery = DB.select(query);
+
+            for (Object[] objects : apprenticeQuery) {
+                int conID = (int) objects[0];
+                int companyID = (int) objects[1];
+                String name = (String) objects[2];
+                String email = (String) objects[3];
+                String phoneNo = (String) objects[4];
+
+                Contacts contacts = new Contacts(conID, companyID, name, email, phoneNo);
+
+                retrievedContacts.add(contacts);
+            }
+
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+
+
+        }
+        return retrievedContacts;
 
     }
 }
