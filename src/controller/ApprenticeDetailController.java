5cd1c1027b812fd695ac1a2a422e926633ccafd3
InLe0 <rockeater0@gmail.com>
2019-06-11 00:43:03 +0100

    Apprentice Detail

--- src/controller/ApprenticeDetailController.java
+++ src/controller/ApprenticeDetailController.java
@@ -1,23 +1,33 @@
 package controller;
 
+import Domain.Apprentices;
+import Persistence.DBFacade;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.TextArea;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class ApprenticeDetailController  extends Controller implements Initializable {
+public class ApprenticeDetailController extends Controller implements Initializable {
     FXMLLoader fxmlLoader;
     String title;
     String username;
+    @FXML
+    TextArea apprenticePersInfoArea;
+    @FXML
+    TextArea ApprenticePendingInfo;
+    Integer apprenticeID;
+
     public void backToApprenticeOverviewHandle(ActionEvent actionEvent) throws IOException {
         title = "Employee Overview";
         fxmlLoader = new FXMLLoader(getClass().getResource("../fxml/apprentice Overview.fxml"));
-        fxmlLoading(fxmlLoader,title);
-        ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+        fxmlLoading(fxmlLoader, title);
+        ((Node) (actionEvent.getSource())).getScene().getWindow().hide();
     }
 
     public void ApprenticeExportHandle(ActionEvent actionEvent) {
@@ -29,19 +39,23 @@ public class ApprenticeDetailController  extends Controller implements Initializ
     public void ApprenticeEditHandle(ActionEvent actionEvent) throws IOException {
         title = "Employee Modification";
         fxmlLoader = new FXMLLoader(getClass().getResource("../fxml/apprentice modification.fxml"));
-        fxmlLoading(fxmlLoader,title);
-        ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+        fxmlLoading(fxmlLoader, title);
+        ((Node) (actionEvent.getSource())).getScene().getWindow().hide();
     }
 
     public void ApprenticeRemoveHandle(ActionEvent actionEvent) throws IOException {
         title = "Employee Overview";
         fxmlLoader = new FXMLLoader(getClass().getResource("../fxml/apprentice Overview.fxml"));
-        fxmlLoading(fxmlLoader,title);
-        ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+        fxmlLoading(fxmlLoader, title);
+        ((Node) (actionEvent.getSource())).getScene().getWindow().hide();
     }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        String userName = KeeperOfKeys.getLoggedUserNameInstance().currentLoggedUserName().getUserName();
-        System.out.println(userName);
+        username = KeeperOfKeys.getLoggedUserNameInstance().currentLoggedUserName().getUserName();
+        apprenticeID = KeeperOfKeys.getLoggedUserNameInstance().currentApprenticeID().getsApprenticeID();
+        Apprentices view = DBFacade.retrieveApprenticeDetail(apprenticeID).get(0);
+        apprenticePersInfoArea.setText(view.getName() + "\n" + view.getEmail() + "\n" + view.getCPR() + "\n" + view.getPhone() + "\n" + view.getGeneralExpertise() + "\n" + view.getWorkExperience() + "\n");
+
     }
 }
