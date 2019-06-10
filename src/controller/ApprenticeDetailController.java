449562c3c228f7d056d790595979829af72792b8
InLe0 <rockeater0@gmail.com>
2019-06-10 23:31:53 +0100

    Maj. Nothing

--- src/controller/ApprenticeDetailController.java
+++ src/controller/ApprenticeDetailController.java
@@ -1,13 +1,23 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ApprenticeDetailController  extends Controller implements Initializable {
-    public void backToApprenticeOverviewHandle(ActionEvent actionEvent) {
+    FXMLLoader fxmlLoader;
+    String title;
+    String username;
+    public void backToApprenticeOverviewHandle(ActionEvent actionEvent) throws IOException {
+        title = "Employee Overview";
+        fxmlLoader = new FXMLLoader(getClass().getResource("../fxml/apprentice Overview.fxml"));
+        fxmlLoading(fxmlLoader,title);
+        ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
     }
 
     public void ApprenticeExportHandle(ActionEvent actionEvent) {
@@ -16,14 +26,21 @@ public class ApprenticeDetailController  extends Controller implements Initializ
     public void ApprenticePrintHandle(ActionEvent actionEvent) {
     }
 
-    public void ApprenticeEditHandle(ActionEvent actionEvent) {
+    public void ApprenticeEditHandle(ActionEvent actionEvent) throws IOException {
+        title = "Employee Modification";
+        fxmlLoader = new FXMLLoader(getClass().getResource("../fxml/apprentice modification.fxml"));
+        fxmlLoading(fxmlLoader,title);
+        ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
     }
 
-    public void ApprenticeRemoveHandle(ActionEvent actionEvent) {
+    public void ApprenticeRemoveHandle(ActionEvent actionEvent) throws IOException {
+        title = "Employee Overview";
+        fxmlLoader = new FXMLLoader(getClass().getResource("../fxml/apprentice Overview.fxml"));
+        fxmlLoading(fxmlLoader,title);
+        ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
     }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        String userName = KeeperOfKeys.getLoggedUserNameInstance().currentUserName().getUserName();
-        System.out.println(userName);
+        username = KeeperOfKeys.getLoggedUserNameInstance().currentUserName().getUserName();
     }
 }
