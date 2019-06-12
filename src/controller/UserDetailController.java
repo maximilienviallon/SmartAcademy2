99d97c4d3e803f8aa046e583e992a95749d742cf
maximilienviallon <42693169+maximilienviallon@users.noreply.github.com>
2019-06-12 03:01:13 +0200

    user detail set up

--- src/controller/UserDetailController.java
+++ src/controller/UserDetailController.java
@@ -1,13 +1,28 @@
 package controller;
 
+import Domain.Users;
+import Persistence.DBFacade;
 import javafx.event.ActionEvent;
-import javafx.fxml.Initializable;
 
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class UserDetailController  extends Controller implements Initializable {
-    public void backToUserOverviewHandle(ActionEvent actionEvent) {
+
+
+    String title;
+    FXMLLoader fxmlLoader;
+    String username;
+    public void backToUserOverviewHandle(ActionEvent actionEvent) throws IOException {
+        title = "User Overview";
+        fxmlLoader = new FXMLLoader(getClass().getResource("../fxml/user Overview.fxml"));
+        fxmlLoading(fxmlLoader, title);
+        ((Node) (actionEvent.getSource())).getScene().getWindow().hide();
     }
 
     public void UserExportHandle(ActionEvent actionEvent) {
@@ -16,14 +31,23 @@ public class UserDetailController  extends Controller implements Initializable {
     public void UserPrintHandle(ActionEvent actionEvent) {
     }
 
-    public void UserEditHandle(ActionEvent actionEvent) {
+    public void UserEditHandle(ActionEvent actionEvent) throws IOException {
+        title = "User Modification";
+        fxmlLoader = new FXMLLoader(getClass().getResource("../fxml/user modification.fxml"));
+        fxmlLoading(fxmlLoader, title);
+        ((Node) (actionEvent.getSource())).getScene().getWindow().hide();
     }
 
-    public void UserRemoveHandle(ActionEvent actionEvent) {
+    public void UserRemoveHandle(ActionEvent actionEvent) throws IOException {
+        title = "USer Overview";
+        fxmlLoader = new FXMLLoader(getClass().getResource("../fxml/user Overview.fxml"));
+        fxmlLoading(fxmlLoader, title);
+        ((Node) (actionEvent.getSource())).getScene().getWindow().hide();
     }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         String userName = KeeperOfKeys.getLoggedUserNameInstance().currentLoggedUserName().getUserName();
-        System.out.println(userName);
+        username = KeeperOfKeys.getLoggedUserNameInstance().currentUserName().getsUserName();
+        Users view = DBFacade.retrieveUsers().get(0);
     }
 }
